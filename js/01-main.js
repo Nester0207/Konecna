8736221a37744fe827b52de31fc2f6982b6a2444
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2024-08-08 14:39:03 +0200

    commit dva

---
+++ js/01-main.js
@@ -0,0 +1,8 @@
+while(true) {
+    let odpovedKlienta = prompt("Jak se jmenujete?").toLowerCase();
+    if (odpovedKlienta != "martin" && odpovedKlienta != "martin"){
+        alert("Zadali jste spatnou odpoved, zkuste to znova");
+    }else{
+        break;
+    }
+}
